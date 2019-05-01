4f4aa26d81f918ccf2c678b9fd0216d654fa12d6
Alex Oskotsky <aoskotsky@amplify.com>
2019-04-30 23:54:49 -0400

    update python versions to include python 3.7

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='',
     author='Amplify Education',
