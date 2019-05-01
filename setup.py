525ca775e3478d43a5305663e4536868ce37b0e3
Alex Oskotsky <aoskotsky@amplify.com>
2019-05-01 00:00:24 -0400

    add more classifiers

--- setup.py
+++ setup.py
@@ -50,7 +50,11 @@ setup(
     long_description=get_long_description(),
     # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
