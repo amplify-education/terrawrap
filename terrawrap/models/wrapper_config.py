f26bd60f27c8030021351de8c25ec7b3714635c1
Alex Oskotsky <aoskotsky@amplify.com>
2019-05-13 16:06:35 -0400

    fix lint errors

--- terrawrap/models/wrapper_config.py
+++ terrawrap/models/wrapper_config.py
@@ -2,7 +2,7 @@
 # TODO: convert these classes to dataclasses once we drop support for Python3.6
 
 from enum import Enum
-from typing import Dict, Union
+from typing import Dict
 
 import jsons
 
@@ -25,19 +25,22 @@ class SSMEnvVarConfig(AbstractEnvVarConfig):
         self.path = path
 
 
-def env_var_deserializer(dict, cls, **kwargs):
+# pylint: disable=unused-argument
+def env_var_deserializer(obj_dict, cls, **kwargs):
     """convert a dict to a subclass of AbstractEnvVarConfig"""
-    if dict['source'] == EnvVarSource.SSM.value:
-        return SSMEnvVarConfig(dict['path'])
+    if obj_dict['source'] == EnvVarSource.SSM.value:
+        return SSMEnvVarConfig(obj_dict['path'])
+
+    raise RuntimeError('Invalid Source')
 
 
-jsons.set_deserializer(env_var_deserializer, SSMEnvVarConfig)
+jsons.set_deserializer(env_var_deserializer, AbstractEnvVarConfig)
 
 
 # pylint: disable=missing-docstring
 class WrapperConfig:
     def __init__(self, configure_backend: bool = True, pipeline_check: bool = True,
-                 envvars: Dict[str, SSMEnvVarConfig] = None,
+                 envvars: Dict[str, AbstractEnvVarConfig] = None,
                  resolved_envvars: Dict[str, str] = None):
         self.configure_backend = configure_backend
         self.pipeline_check = pipeline_check
