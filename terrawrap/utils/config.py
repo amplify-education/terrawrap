38519fe0ca44315e546ef7f3d0a7e5a4bd0da52e
Alex Oskotsky <aoskotsky@amplify.com>
2019-08-20 14:37:04 -0400

    use python_hcl2 instead of pyhcl

--- terrawrap/utils/config.py
+++ terrawrap/utils/config.py
@@ -4,7 +4,7 @@ import re
 import subprocess
 from typing import Dict, List
 
-import hcl
+import hcl2
 import jsons
 import yaml
 from ssm_cache import SSMParameterGroup
@@ -155,6 +155,7 @@ def parse_variable_files(variable_files: List[str]) -> Dict[str, str]:
 
     for variable_file in variable_files:
         with open(variable_file) as var_file:
-            variables.update(hcl.load(var_file))
+            flat_vars = {key: values[0] for key, values in hcl2.load(var_file).items()}
+            variables.update(flat_vars)
 
     return variables
--- terrawrap/version.py
+++ terrawrap/version.py
@@ -1,4 +1,4 @@
 """Place of record for the package version"""
 
-__version__ = "0.4.16"
+__version__ = "0.4.17"
 __git_hash__ = "GIT_HASH"
