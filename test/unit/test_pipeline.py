d984e662e6478933ae8e038774a879362c0beeef
Johnathan Constance <jconstance@amplify.com>
2019-09-16 17:44:12 -0400

    Optionally Allow Less Printing for tf_apply
    Added a `--print-only-changes` option to tf_apply, that will change the
    behavior of tf_apply such that it only prints Terraform output for
    directories that actually changed.
    
    Additionally, tf_apply will now not apply a directory if the plan comes
    back clean for it.
    
    Additionally, tf_apply will now run init and the desired command in one
    operation, which should make applies overall faster and reduce some
    needless printing that was going on.

--- test/unit/test_pipeline.py
+++ test/unit/test_pipeline.py
@@ -20,7 +20,7 @@ class TestPipeline(TestCase):
     @patch('terrawrap.models.pipeline.PipelineEntry')
     def test_execute(self, pipeline_entry_class):
         """Test that executing a pipeline will call init and then the command"""
-        pipeline_entry_class.return_value.execute.return_value = (0, ['Success'])
+        pipeline_entry_class.return_value.execute.return_value = (0, ['Success'], True)
 
         pipeline = Pipeline('plan', 'pipelines/test.csv')
 
@@ -28,5 +28,5 @@ class TestPipeline(TestCase):
 
         self.assertEqual(
             pipeline_entry_class.return_value.execute.mock_calls,
-            [call('init', debug=False), call('plan', debug=False)]
+            [call('plan', debug=False)]
         )
--- test/unit/test_pipeline_entry.py
+++ test/unit/test_pipeline_entry.py
@@ -15,10 +15,11 @@ class TestPipelineEntry(TestCase):
         exec_command.side_effect = [(0, ['Success'])]
 
         entry = PipelineEntry('/var', [])
-        exit_code, stdout = entry.execute('plan')
+        exit_code, stdout, changes_detected = entry.execute('plan')
 
         self.assertEqual(exit_code, 0)
-        self.assertEqual(stdout, ['Success'])
+        self.assertEqual(stdout, ['Success', '\n'])
+        self.assertEqual(changes_detected, False)
 
     @patch('terrawrap.models.pipeline_entry.execute_command')
     def test_execute_fail(self, exec_command):
@@ -26,7 +27,39 @@ class TestPipelineEntry(TestCase):
         exec_command.side_effect = [(1, ['Fail'])]
 
         entry = PipelineEntry('/var', [])
-        exit_code, stdout = entry.execute('plan')
+        exit_code, stdout, changes_detected = entry.execute('plan')
 
         self.assertEqual(exit_code, 1)
         self.assertEqual(stdout, ['Fail'])
+        self.assertEqual(changes_detected, True)
+
+    @patch('terrawrap.models.pipeline_entry.execute_command')
+    def test_execute_apply_changes(self, exec_command):
+        """Test executing apply with changes"""
+        exec_command.side_effect = [
+            (0, ['Success']),
+            (2, ['Something changed']),
+            (0, ['Success']),
+        ]
+
+        entry = PipelineEntry('/var', [])
+        exit_code, stdout, changes_detected = entry.execute('apply')
+
+        self.assertEqual(exit_code, 0)
+        self.assertEqual(stdout, ['Success', '\n', 'Something changed', '\n', 'Success'])
+        self.assertEqual(changes_detected, True)
+
+    @patch('terrawrap.models.pipeline_entry.execute_command')
+    def test_execute_apply_no_changes(self, exec_command):
+        """Test executing apply with no changes"""
+        exec_command.side_effect = [
+            (0, ['Success']),
+            (0, ['Nothing changed']),
+        ]
+
+        entry = PipelineEntry('/var', [])
+        exit_code, stdout, changes_detected = entry.execute('apply')
+
+        self.assertEqual(exit_code, 0)
+        self.assertEqual(stdout, ['Success', '\n', 'Nothing changed'])
+        self.assertEqual(changes_detected, False)
