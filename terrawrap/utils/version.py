1eb7dae9ece5e30f29df37cb654389ddd260881d
Johnathan Constance <jconstance@amplify.com>
2019-09-17 14:55:00 -0400

    Switch to DiskCache Instead of Rolling Our Own

--- terrawrap/utils/version.py
+++ terrawrap/utils/version.py
@@ -2,13 +2,15 @@
 import sys
 import tempfile
 import os
-import json
-from typing import Dict, Any
-from datetime import datetime
 from time import sleep
 
 import requests
 from packaging import version
+from diskcache import Cache
+
+
+ONE_DAY_IN_SECONDS = 60 * 60 * 24
+cache = Cache(os.path.join(tempfile.gettempdir(), "terrawrap_version_cache"))
 
 
 def version_check(current_version: str):
@@ -40,65 +42,14 @@ def version_check(current_version: str):
     return False
 
 
+# We supply current_version so that the cache is invalidated when you install a new version of Terrawrap.
+# pylint: disable=unused-argument
+@cache.memoize(expire=ONE_DAY_IN_SECONDS)
 def get_latest_version(current_version: str) -> str:
     """
     Get the latest version of Terrawrap from Pypi. Caches this lookup for one day locally.
     :param current_version: The current version of Terrawrap.
     :return: The latest version of Terrawrap, potentially delayed by one day.
     """
-    cache_file_path = os.path.join(tempfile.gettempdir(), "terrawrap_version_cache")
-    cached_data = get_cache(cache_file_path=cache_file_path)
-
-    current_version_changed = current_version != cached_data.get("current_version")
-    cache_outdated = (datetime.utcnow() - cached_data.get("timestamp", datetime.utcnow())).days > 0
-
-    if not cached_data or current_version_changed or cache_outdated:
-        response = requests.get("https://pypi.python.org/pypi/terrawrap/json").json()
-        latest_version = response["info"]["version"]
-        set_cache(
-            cache_file_path=cache_file_path,
-            latest_version=latest_version,
-            current_version=current_version,
-        )
-        return latest_version
-
-    return cached_data["latest_version"]
-
-
-def get_cache(cache_file_path: str) -> Dict[str, Any]:
-    """
-    Get the cached data for Terrawrap.
-    :param cache_file_path: The path to the cache file.
-    :return: The cached data, or an empty dictionary if no cache was found.
-    """
-    if not os.path.exists(cache_file_path):
-        return {}
-
-    try:
-        with open(cache_file_path, "r") as cache_file:
-            data = json.load(cache_file)
-            data["timestamp"] = datetime.strptime(
-                data["timestamp"],
-                "%Y-%m-%dT%H:%M:%S.%f",
-            )
-            return data
-    except json.decoder.JSONDecodeError:
-        return {}
-
-
-def set_cache(cache_file_path: str, latest_version: str, current_version: str):
-    """
-    Set the cached data for Terrawrap.
-    :param cache_file_path: The path to the cache file.
-    :param latest_version: The latest version of Terrawrap.
-    :param current_version: The current version of Terrawrap.
-    """
-    with open(cache_file_path, "w") as cache_file:
-        json.dump(
-            {
-                "timestamp": datetime.utcnow().isoformat(),
-                "latest_version": latest_version,
-                "current_version": current_version,
-            },
-            cache_file,
-        )
+    response = requests.get("https://pypi.python.org/pypi/terrawrap/json").json()
+    return response["info"]["version"]
--- test/unit/test_version.py
+++ test/unit/test_version.py
@@ -1,18 +1,16 @@
 """Test version utils"""
-import json
-import os
-import tempfile
-
-from datetime import datetime, timedelta
 from unittest import TestCase
 
-from mock import patch, MagicMock, ANY
+from mock import patch, MagicMock
 
-from terrawrap.utils.version import version_check, get_latest_version, get_cache, set_cache
+from terrawrap.utils.version import version_check, get_latest_version, cache
 
 
 class TestVersion(TestCase):
     """Test version utils"""
+
+    def setUp(self) -> None:
+        cache.clear()
 
     @patch("terrawrap.utils.version.sleep", MagicMock())
     @patch("terrawrap.utils.version.get_latest_version")
@@ -71,40 +69,11 @@ class TestVersion(TestCase):
         )
 
     @patch("requests.get")
-    @patch("terrawrap.utils.version.get_cache")
-    def test_get_latest_version_valid_cache(self, mock_get_cache, mock_requests_get):
+    def test_get_latest_version_valid_cache(self, mock_get):
         """VersionUtils get latest version with valid cache"""
         current_version = "1.0.0"
-        latest_version = "1.0.0"
-        mock_cache = {
-            "timestamp": datetime.utcnow(),
-            "latest_version": current_version,
-            "current_version": latest_version,
-        }
-        mock_get_cache.return_value = mock_cache
-
-        response = get_latest_version(current_version=current_version)
-
-        self.assertEqual(
-            response,
-            latest_version,
-        )
-        mock_requests_get.assert_not_called()
-
-    @patch("requests.get")
-    @patch("terrawrap.utils.version.set_cache")
-    @patch("terrawrap.utils.version.get_cache")
-    def test_get_latest_version_old_cache(self, mock_get_cache, mock_set_cache, mock_requests_get):
-        """VersionUtils get latest version with old cache"""
-        current_version = "1.0.0"
         latest_version = "1.0.1"
-        mock_cache = {
-            "timestamp": datetime.utcnow() - timedelta(days=1),
-            "latest_version": current_version,
-            "current_version": current_version,
-        }
-        mock_get_cache.return_value = mock_cache
-        mock_requests_get.return_value.json.return_value = {
+        mock_get.return_value.json.return_value = {
             "info": {
                 "version": latest_version
             }
@@ -115,78 +84,4 @@ class TestVersion(TestCase):
         self.assertEqual(
             response,
             latest_version,
-        )
-        mock_set_cache.assert_called_once_with(
-            cache_file_path=ANY,
-            latest_version=latest_version,
-            current_version=current_version,
-        )
-
-    def test_get_cache_no_file(self):
-        """VersionUtils get cache with no file"""
-        cache_file_path = os.path.join(os.path.dirname(__file__), "does_not_exist_cache")
-
-        response = get_cache(cache_file_path=cache_file_path)
-
-        self.assertEqual(
-            response,
-            {},
-        )
-
-    @patch("json.load")
-    def test_get_cache_bad_file(self, mock_json_load):
-        """VersionUtils get cache with malformed file"""
-        cache_file_path = os.path.join(os.path.dirname(__file__), "invalid_json_cache")
-        mock_json_load.side_effect = json.decoder.JSONDecodeError(
-            MagicMock(),
-            MagicMock(),
-            MagicMock(),
-        )
-
-        response = get_cache(cache_file_path=cache_file_path)
-
-        self.assertEqual(
-            response,
-            {},
-        )
-
-    def test_get_cache_good_file(self):
-        """VersionUtils get cache with good file"""
-        cache_file_path = os.path.join(os.path.dirname(__file__), "valid_cache")
-        current_version = "1.0.0"
-        latest_version = "1.0.1"
-
-        response = get_cache(cache_file_path=cache_file_path)
-
-        self.assertEqual(
-            response,
-            {
-                "timestamp": datetime(2019, 9, 17, 11, 53, 13, 31919),
-                "latest_version": latest_version,
-                "current_version": current_version,
-            }
-        )
-
-    @patch("terrawrap.utils.version.datetime")
-    @patch("json.dump")
-    def test_set_cache(self, mock_dump, mock_datetime):
-        """VersionUtils set cache"""
-        # pylint: disable=unused-variable
-        cache_file, cache_file_path = tempfile.mkstemp()
-        current_version = "1.0.0"
-        latest_version = "1.0.1"
-
-        set_cache(
-            cache_file_path=cache_file_path,
-            latest_version=latest_version,
-            current_version=current_version,
-        )
-
-        mock_dump.assert_called_once_with(
-            {
-                "timestamp": mock_datetime.utcnow.return_value.isoformat.return_value,
-                "latest_version": latest_version,
-                "current_version": current_version,
-            },
-            ANY
         )
