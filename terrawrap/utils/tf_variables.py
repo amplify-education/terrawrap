e408c03f22cef9013946d8ddc88d084bc2f2b6f5
Alex Oskotsky <aoskotsky@amplify.com>
2021-07-14 14:43:17 -0400

    fix unit tests

--- terrawrap/utils/tf_variables.py
+++ terrawrap/utils/tf_variables.py
@@ -28,10 +28,6 @@ def get_auto_vars(root_directory: str) -> Dict[str, Set[Variable]]:
             with open(current_dir + '/' + file_name, 'r') as file:
                 variables = hcl2.load(file)
                 for key, value in variables.items():
-                    # python-hcl2 will always return a list for each variable value in case
-                    # a variable is defined multiple times in the same time
-                    # We will just use the last appearance of a variable
-                    value = value[-1]
                     auto_vars[os.path.join(current_dir, file_name)].add(Variable(key, _make_hashable(value)))
 
     return dict(auto_vars)
