7505f1b948de075b7f19f76887f921c3a9402a0c
Brandon Montijo <59846295+brandonmontijo@users.noreply.github.com>
2022-05-18 08:43:55 -0500

    Add `audit_api_url` and `cwd` parameters

--- terrawrap/models/graph_entry.py
+++ terrawrap/models/graph_entry.py
@@ -80,6 +80,8 @@ class GraphEntry(Entry):
             print_output=False,
             capture_stderr=True,
             env=command_env,
+            audit_api_url=self.wrapper_config.audit_api_url,
+            cwd=self.abs_path
         )
         if init_exit_code != 0:
             self.state = "Failed"
