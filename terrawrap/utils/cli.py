2559f25e7371314c95a2cdb66d69cf0546891352
Brandon Montijo <59846295+brandonmontijo@users.noreply.github.com>
2022-06-16 14:11:35 -0500

    Update `cli`

--- terrawrap/utils/cli.py
+++ terrawrap/utils/cli.py
@@ -118,6 +118,16 @@ def execute_command(
             break
 
         if time_passed >= timeout:
+            if audit_api_url and kwargs['cwd']:
+                # Call _post_audit_info again, this time to update the 'in progress' entry with new status and output
+                _post_audit_info(
+                    audit_api_url=audit_api_url,
+                    path=kwargs['cwd'],
+                    exit_code=exit_code,
+                    stdout=stdout,
+                    start_time=start_time,
+                    update=True
+                )
             raise TimeoutError(f'Timed out retrying {args} command')
 
         time_passed = jitter.backoff()
